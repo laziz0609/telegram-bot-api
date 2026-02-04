3fdcaab3eae81649af324fea5720a6af656cb071
laziz0609 <laziz20060927@gmail.com>
2026-02-05 00:16:19 +0500

    add asynxronus, echo bot version1.2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import requests
-from pprint import pprint
+import asyncio
+import aiohttp
 
 from config import TOKEN, ADMINS
 
@@ -9,81 +9,86 @@ class Bot:
     
     def __init__(self, token: str):
         self.token = token
+        self.session = aiohttp.ClientSession()
         self.BASE_URL = F'https://api.telegram.org/bot{token}'
-        self.offset = int
+        self.offset = 0
         self.limit = 5
     
      
-    def get_updates(self) -> list[dict] | bool:
-        get_url = f'{self.BASE_URL}/getUpdates'
+    async def get_updates(self) -> list[dict] | bool:
+        get_updates_url = f'{self.BASE_URL}/getUpdates'
         
         params = {
         "offset": self.offset,
         "limit": self.limit 
         }
-        response = requests.get(get_url, params=params)
-
-        if response.status_code == 200:
-            return response.json()["result"]
-        return False
+        async with self.session.get(get_updates_url, params=params) as response: 
+            if response.status == 200:
+                data = await response.json()
+                return data["result"]
+            return False
     
     
-    def copy_message(self, chat_id: int | str,  from_chat_id: int | str, message_id: int, ) -> bool:
-        get_url = f'{self.BASE_URL}/copyMessage'
+    async def copy_message(self, chat_id: int | str,  from_chat_id: int | str, message_id: int, ) -> bool:
+        copy_message_url = f'{self.BASE_URL}/copyMessage'
         
         params = {
             "chat_id": chat_id,
             "from_chat_id": from_chat_id,
             "message_id": message_id
         }
-        response = requests.get(get_url, params=params)
-
-        return True if response.status_code == 200 else False
+        async with self.session.post(copy_message_url, params=params) as response: 
+            return True if response.status == 200 else False
     
     
-    def send_message(self, chat_id: int | str, text: str) -> None:
-        get_url = f'{self.BASE_URL}/sendMessage'
+    
+    async def send_message(self, chat_id: int | str, text: str) -> bool:
+        send_message_url = f'{self.BASE_URL}/sendMessage'
         
         params = {
             "chat_id": chat_id,
             "text": text
         }
-        requests.get(get_url, params=params)
+        async with self.session.post(send_message_url, params=params) as res: 
+            return True if res.status == 200 else False
     
     
-    def main(self) -> None:
+    
+    async def main(self) -> None:
         
         while True:
-            updates = self.get_updates()
+            updates = await self.get_updates()
             
             if updates:
                 for update in updates:
                     chat_id = update["message"]["chat"]["id"]
                     message_id = update["message"]["message_id"]
                     
-                    copy_status = self.copy_message(chat_id=chat_id, from_chat_id=chat_id, message_id=message_id)
+                    copy_status = await self.copy_message(chat_id=chat_id, from_chat_id=chat_id, message_id=message_id)
                     if not copy_status:
-                        self.send_message(chat_id=chat_id, text="yaxshi")
+                        await self.send_message(chat_id=chat_id, text="yaxshi")
                         
                 self.offset = updates[-1]["update_id"] + 1
                     
                 
 
-def notify(admins: list, bot: Bot):
+async def notify(admins: list, bot: Bot):
     try:
         for admin in admins:
-            bot.send_message(admin, "Bot ishga tushdi")
-    
+            status = await bot.send_message(admin, "Bot ishga tushdi")
+            if not status:
+                print("xabar adminga jo'natilmadi")
     except Exception as e:
         print("ERROR   \n {e}")
     
               
 
-def main() -> None:
+async def main() -> None:
     bot = Bot(TOKEN)
     
-    bot.main()
-    notify(ADMINS, bot)
+    await notify(ADMINS, bot)
+    await bot.main()
     
     
-main()
+    
+asyncio.run(main())
